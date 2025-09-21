b38fcd3690187428ac44d885b50543a0cd35cd2d
Gopalakrishnan <mgopalakrishnan486@gmail.com>
2025-09-21 18:39:18 +0530

    Usage of HttpContextAccessor

---
+++ 2. Fundamentals/http-context/src/Api/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        public ValuesController()
+        {
+
+        }
+
+        [HttpGet]
+
+        public IActionResult Get([FromServices] IHttpContextAccessor httpContextAccessor)
+        {
+            var request = HttpContext.Request;
+
+            var request2 = httpContextAccessor?.HttpContext?.Request;
+
+            var response = httpContextAccessor?.HttpContext?.Response;
+
+            ClaimsPrincipal? principal = httpContextAccessor?.HttpContext?.User;
+
+            return Ok(request.Path + " " + request2.Path);
+        }
+    }
+}
