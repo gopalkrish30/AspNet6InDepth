c4d991751373825cf432e17eac38f7721d6e14b0
Gopalakrishnan <mgopalakrishnan486@gmail.com>
2025-09-24 08:58:51 +0530

    Content Negotiation workflow in .NET core

---
+++ 2. Fundamentals/content-negotiation/src/Api/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        List<Item> list = new List<Item>
+        {
+            new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = "Television",
+                price = "25,000"
+            },
+            new Item {
+                Id = Guid.NewGuid(),
+                Name = "Iphone",
+                price = "60,000"
+            }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(list);
+        }
+
+        [HttpGet("getbyid/{id}")]
+        public IActionResult Get2(Guid id)
+        {
+            Item item = list.FirstOrDefault(x => x.Id == id);   
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult<List<Item>> Post([FromBody] Item item)
+        {
+            list.Add(item);
+            return CreatedAtAction(nameof(Get2), new { id = item.Id }, item);
+        }
+    }
+}
---
+++ 2. Fundamentals/content-negotiation/src/Api/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string price { get; set; }
+    }
+}
